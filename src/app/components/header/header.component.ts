9a37e39faadfb95bd35bdc4c07106d36611136c3
airtonvasconcelosjr <airton.vasconcelosjr@gmail.com>
2023-07-14 14:19:21 -0300

    Inserting header options

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit } from '@angular/core';
+import { faSearch, faShoppingCart, faUser, faList, faClock, faLocation, faHome, faListAlt, faFire } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   searchIcon = faSearch;
+  faShoppingCart = faShoppingCart;
+  faUser = faUser;
+  faList = faList;
+  faClock = faClock;
+  faLocation = faLocation;
+  faHome = faHome;
+  faListAlt = faListAlt;
+  faFire = faFire;
+
+  ngOnInit() {
+    window.addEventListener('scroll', function() {
+      var header = document.querySelector('.header');
+      var scrollPosition = window.scrollY;
+
+      if (scrollPosition > 0) {
+        header?.classList.add('fixed');
+      } else {
+        header?.classList.remove('fixed');
+      }
+    });
+  }
 }
